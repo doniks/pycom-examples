10d49a7a0529f2a0831af9412a08298262af77fb
Peter Putz <peter@pycom.io>
2020-02-13 20:59:34 +0100

    do_scan - False

--- wlan.py
+++ wlan.py
@@ -4,27 +4,39 @@ import socket
 import ssl
 import time
 
+start = time.time()
+
 wlan = WLAN(mode=WLAN.STA)
 
 # fill in !!!
 ssid = 'XXX'
 passwd = 'XXX'
+sec    = 3
+
+do_scan = False
+
+def connect(ssid, auth):
+    print("Connecting ...", end="")
+    wlan.connect(ssid, auth, timeout=5000)
+    while not wlan.isconnected():
+        time.sleep_ms(200)
+        print(".", end="")
+    print('WLAN connection succeeded after', time.time() - start, "seconds")
 
 if wlan.isconnected():
     print("Already connected")
 else:
-    print("Scanning ...")
-    nets = wlan.scan()
-    for net in nets:
-        if net.ssid == ssid:
-            print('Network', ssid, 'found. Connecting ...')
-            wlan.connect(net.ssid, auth=(net.sec, passwd), timeout=5000)
-            while not wlan.isconnected():
-                #machine.idle() # save power while waiting
-                time.sleep_ms(200)
-                print(".", end="")
-            print('WLAN connection succeeded!')
-            break
+    if do_scan:
+        print("Scanning ...")
+        nets = wlan.scan()
+        for net in nets:
+            if net.ssid == ssid:
+                print('Network', ssid, 'found after', time.time() - start, 'seconds')
+                connect(net.ssid, auth=(net.sec, passwd))
+                break
+    else:
+        connect(ssid, auth=(sec, passwd))
+        pass
 
 print("WLAN connected:", wlan.ssid(), wlan.ifconfig() )
 print(wlan.ifconfig()[0] )
