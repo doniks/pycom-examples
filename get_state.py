e9ad9315c4a853bd7e820ab3e26af8f2771db108
Peter Putz <peter@pycom.io>
2020-07-06 14:42:51 +0200

    support older firmwares with try/excepts

--- get_state.py
+++ get_state.py
@@ -1,11 +1,22 @@
 import os
-import ubinascii
+import binascii
 import machine
+
+def print_wifi_mode(m):
+    if m == WLAN.AP:
+        print("AP", end='')
+    elif m == WLAN.STA:
+        print("STA", end='')
+    elif m == WLAN.STA_AP:
+        print("STA_AP", end='')
 
 
 print("===== machine ====================================")
-print("unique_id", ubinascii.hexlify(machine.unique_id()))
-print("temperature", machine.temperature())
+print("unique_id", binascii.hexlify(machine.unique_id()))
+try:
+    print("temperature", machine.temperature())
+except:
+    pass
 # print("rng", machine.rng())
 
 print("===== os =========================================")
@@ -16,25 +27,47 @@ print("uname", os.uname())
 
 import pycom
 print("===== pycom ======================================")
-# print("bootmgr", pycom.bootmgr())
-print("partition", pycom.bootmgr()[0])
-print("fs_type", pycom.bootmgr()[1])
-print("safeboot", pycom.bootmgr()[2])
-print("status", pycom.bootmgr()[3])
-print("ota_slot", hex(pycom.ota_slot()))
-if (pycom.ota_slot() == 0x210000):
-    print("ota_slot is", "'ota_0' in 'new' 8MB layout")
-elif (pycom.ota_slot() == 0x1A0000):
-    print("ota_slot is", "'ota_0' in 'old' 4MB layout")
-elif (pycom.ota_slot() == 0x10000):
-    print("ota_slot is", "'Factory'")
-else:
-    raise Exception("Unkown ota_slot"+ str(pycom.ota_slot()))
+try:
+    print("free_heap", pycom.get_free_heap())
+    # print("bootmgr", pycom.bootmgr())
+    print("partition", pycom.bootmgr()[0])
+    print("fs_type", pycom.bootmgr()[1])
+    print("free", os.getfree('/flash'))
+    print(pycom.bootmgr()[2]) # safeboot
+    print(pycom.bootmgr()[3]) # status
+    print("ota_slot", hex(pycom.ota_slot()))
+    if (pycom.ota_slot() == 0x210000):
+        print("ota_slot is", "'ota_0' in 'new' 8MB layout")
+    elif (pycom.ota_slot() == 0x1A0000):
+        print("ota_slot is", "'ota_0' in 'old' 4MB layout")
+    elif (pycom.ota_slot() == 0x10000):
+        print("ota_slot is", "'Factory'")
+    else:
+        raise Exception("Unkown ota_slot"+ str(pycom.ota_slot()))
+except:
+    pass
 
-print("smart_config_on_boot", pycom.smart_config_on_boot())
-print("pybytes_on_boot", pycom.pybytes_on_boot())
+try:
+    print("smart_config_on_boot", pycom.smart_config_on_boot())
+except:
+    pass
+try:
+    print("pybytes_on_boot", pycom.pybytes_on_boot())
+except:
+    pass
 
-print("wifi_on_boot", pycom.wifi_on_boot())
+try:
+    print("wifi_on_boot", pycom.wifi_on_boot())
+except:
+    pass
+from network import WLAN
+try:
+    m = pycom.wifi_mode_on_boot()
+    print("wifi_mode_on_boot", m, end=" ")
+    print_wifi_mode(m)
+    print()
+except:
+    pass
 try:
     print("wifi_ssid", pycom.wifi_ssid())
     print("wifi_pwd", pycom.wifi_pwd())
@@ -51,20 +84,30 @@ except:
 
 
 print("===== wlan =======================================")
-from network import WLAN
 wlan = WLAN()
+try:
+    print("sta_mac", binascii.hexlify(wlan.mac().sta_mac))
+    print("ap_mac", binascii.hexlify(wlan.mac().ap_mac))
+except:
+    print("mac", binascii.hexlify(wlan.mac()))
 print("is_connected", wlan.isconnected())
-print("bssid", ubinascii.hexlify(wlan.bssid()))
-print("country", wlan.country())
+print("ssid", wlan.ssid())
+print("bssid", binascii.hexlify(wlan.bssid()))
+try:
+    print("country", wlan.country())
+except:
+    pass
 print("ifconfig", wlan.ifconfig())
 print('IP:', wlan.ifconfig()[0])
-print("mode", wlan.mode(), "(STA=", WLAN.STA, "AP=", WLAN.AP, ")")
+print("mode", wlan.mode(), end=' ')
+print_wifi_mode(wlan.mode())
+print()
 
 try:
     print("===== lora =======================================")
     from network import LoRa
     lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
-    print("mac", ubinascii.hexlify(lora.mac()))
+    print("mac", binascii.hexlify(lora.mac()))
     print(lora.frequency())
     print(lora.has_joined())
     print(lora.tx_power())
@@ -77,9 +120,9 @@ try:
     print("===== sigfox =====================================")
     from network import Sigfox
     sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
-    print("id", ubinascii.hexlify(sigfox.id()))
-    print("mac", ubinascii.hexlify(sigfox.mac()))
-    print("pac", ubinascii.hexlify(sigfox.pac()))
+    print("id", binascii.hexlify(sigfox.id()))
+    print("mac", binascii.hexlify(sigfox.mac()))
+    print("pac", binascii.hexlify(sigfox.pac()))
     print("frequencies", sigfox.frequencies())
 except:
     pass
@@ -92,6 +135,18 @@ try:
     print("iccid", lte.iccid())
     print("is_connected", lte.isconnected())
     print("ue_coverage", lte.ue_coverage())
-    # print("time", lte.time())
+    def send_at_cmd_pretty(cmd):
+        return lte.send_at_cmd(cmd).replace('\r', '').strip().replace('\n\n','\n')
+    print('AT+CGCONTRDP=1', send_at_cmd_pretty('AT+CGCONTRDP=1'))
+    print('AT!="config"', send_at_cmd_pretty('AT!="ifconfig"'))
+    print("time", lte.time())
+except:
+    pass
+
+try:
+    print("===== eth ========================================")
+    from network import ETH
+    eth = ETH()
+    print("mac", binascii.hexlify(eth.mac()))
 except:
     pass
