f5f9973a73020f89fc7d0d8b5dd36679f72156a9
Peter Putz <peter@pycom.io>
2020-07-06 14:44:17 +0200

    m

--- nvram.py
+++ nvram.py
@@ -1,13 +1,25 @@
 import pycom
 
 key = "nvram_test_key0" # max 15 characters long
+key = "nvram6789abcdefs" # max 15 characters long
 # print(len(key))
-value = 42
-pycom.nvs_set(key, value)
-x = 0
+newvalue = 42
+
+try:
+    oldvalue = pycom.nvs_get(key)
+    print("get", key, oldvalue)
+    newvalue = oldvalue + 1
+except Exception as e:
+    print("Couldn't read", key, e)
+
+print("set", key, newvalue)
+pycom.nvs_set(key, newvalue)
+
+
 x = pycom.nvs_get(key)
-print(x)
-if not x == value:
-    raise "hell"
-pycom.nvs_erase(key)
+print("get", key, x)
+
+# print("erase")
+# pycom.nvs_erase(key)
+
 print("OK")
