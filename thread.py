5dc36fd731bd84c39dac51fd788243fa4358026f
Peter Putz <peter@pycom.io>
2020-07-06 14:45:47 +0200

    crash test

--- thread.py
+++ thread.py
@@ -1,12 +1,32 @@
 import _thread
 import time
+import machine
+
+keep_going = True
 
 def th_func(delay, id):
     ct = 0
-    while True:
+    while keep_going:
         time.sleep(delay)
         print('Running thread id=%d, ct=%d' % (id, ct) )
         ct += 1
+    print('End thread id=%d' %(id))
 
-for i in range(5):
-    _thread.start_new_thread(th_func, (i + 1, i))
+
+def thread_test():
+    n = 20
+    print("Starting %d threads...." %(n))
+    for i in range(n):
+        _thread.start_new_thread(th_func, (i + 1, i))
+        print(i)
+        time.sleep(0.2)
+        machine.info()
+
+    if False:
+        keep_going = False
+
+if __name__ == "__main__":
+    # machine.info()
+    print("sd")
+    thread_test()
+    print("adf")
