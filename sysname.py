e2bbe7995e18c89dd6a0af3097125fd556f118ae
Peter Putz <peter@pycom.io>
2020-07-06 14:45:33 +0200

    m

--- sysname.py
+++ sysname.py
@@ -1,2 +1,4 @@
 import os
 print(os.uname().sysname) # e.g., GPy
+# print(os.uname())
+# (sysname='GPy', nodename='GPy', release='1.20.2.rc9', version='v1.20.1.r2-248-g179ebe8c1-dirty XX 2020-06-17', machine='GPy with ESP32', pybytes='1.5.0')
