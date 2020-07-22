ecdc2d1940f66752269df2738758c5f661dbd9f8
Peter Putz <peter@pycom.io>
2020-07-22 12:25:00 +0200

    sd

--- sdcard.py
+++ sdcard.py
@@ -1,19 +1,28 @@
-from machine import SD
-import os
 
-sd = SD()
-try:
-    os.mount(sd, '/sd')
-except Exception as e:
-    print("Exception while trying to mount (maybe it's mounted already):", e)
+def sd():
+    from machine import SD
+    import os
 
-# check the content
-print(os.listdir('/sd'))
+    sd = SD()
+    try:
+        os.mount(sd, '/sd')
+    except Exception as e:
+        if os.stat('/sd'):
+            # should be ok, probably previously mounted
+            pass
+        else:
+            print("Exception while trying to mount:", e)
 
-# # try some standard file operations
-# f = open('/sd/test.txt', 'w')
-# f.write('Testing SD card write operations')
-# f.close()
-# f = open('/sd/test.txt', 'r')
-# f.readall()
-# f.close()
+    # check the content
+    print('/sd :', os.listdir('/sd'))
+
+    # # try some standard file operations
+    # f = open('/sd/test.txt', 'w')
+    # f.write('Testing SD card write operations')
+    # f.close()
+    # f = open('/sd/test.txt', 'r')
+    # f.readall()
+    # f.close()
+
+if __name__ == "__main__":
+    sd()
