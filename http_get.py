f49512be496539266ed9025b46f2b2de50a4fe64
Peter Putz <peter@pycom.io>
2020-08-11 15:23:17 +0200

    time in ms, param kb, do_print, bugs

--- http_get.py
+++ http_get.py
@@ -2,58 +2,95 @@ import socket
 import time
 def_url = 'http://detectportal.firefox.com/'
 
-def http_get(url = def_url):
-    time_start = time.time()
-    dur_send = 0
-    dur_recv = 0
-    dur_total = 0
+def http_get(url=None, kb=None, verbose=False, timeout_s=5, do_print=False):
+    if not url:
+        if kb is not None:
+            if kb == 0:
+                url = def_url # the smallest file I know
+                print('smallest file:', url)
+            elif kb == 1:
+                url = 'http://ftp.snt.utwente.nl/pub/docs/rfc/rfc1498.json' # 1000 B
+                print('1k file:', url)
+            elif kb == 10:
+                url = 'http://ftp.snt.utwente.nl/pub/docs/rfc/rfc1535.html'
+                print('10k file:', url)
+            elif kb == 100:
+                url = 'http://ftp.snt.utwente.nl/pub/docs/rfc/rfc753.html'
+                print('100k file:', url)
+            elif kb == 1000:
+                url = 'http://ftp.snt.utwente.nl/pub/test/1M'
+                print('1M file', url)
+            elif kb == 10000:
+                url = 'http://ftp.snt.utwente.nl/pub/test/10M'
+            else:
+                raise Exception("Don't have a url that matches kb=", kb)
+            # http_get('http://ftp.snt.utwente.nl/pub/test/100M')
+        else:
+            url = def_url
+
+    time_start_ms = time.ticks_ms()
+    dur_send_ms = 0
+    dur_recv_ms = 0
+    dur_total_s = 0
     bps_send = 0
     bps_recv = 0
     len_recv = 0
     cnt_recv = 0
     success = False
     try:
-        print("http_get(", url, ")")
+        # print("http_get(", url, ")")
         _, _, host, path = url.split('/', 3)
-        print('getaddrinfo')
+        # print('getaddrinfo')
         ip_port = socket.getaddrinfo(host, 80)[0][-1]
-        print('socket')
+        # print('socket')
         s = socket.socket()
-        print('connect')
+        # print('connect')
         s.connect(ip_port)
         request = bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8')
-        time_send = time.time()
-        print('send request')
+        time_send_ms = time.ticks_ms()
+        # print('send request')
         s.send(request)
-        dur_send = time.time() - time_send
-        if dur_send:
-            bps_send = len(request) / dur_send
+        dur_send_ms = time.ticks_ms() - time_send_ms
+        if dur_send_ms:
+            bps_send = len(request) / dur_send_ms / 1000
 
-        print('recv response')
-        time_recv = time.time()
-        s.settimeout(60)
+        # print('recv response')
+        time_recv_ms = time.ticks_ms()
+        s.settimeout(timeout_s) # 10)
         while True:
             data = s.recv(100)
             if data:
                 cnt_recv += 1
                 len_recv += len(data)
-                dur_recv = time.time() - time_recv
-                if dur_recv:
-                    bps_recv = len_recv / dur_recv
-                if cnt_recv % 1000 == 0:
-                    print("received[", time.time(), "]:", len_recv, bps_recv)
+                dur_recv_ms = time.ticks_ms() - time_recv_ms
+                if dur_recv_ms:
+                    dur_recv_s = dur_recv_ms / 1000
+                    bps_recv = len_recv / dur_recv_s
+                    if verbose or dur_recv_s % 5 == 0:
+                        print("received[", time.time(), "]: len=", len_recv, "bps=", bps_recv)
+                    if do_print:
+                        print(str(data, 'utf8'), end='')
+
                 success = True
-                # print(str(data, 'utf8'), end='')
                 pass
             else:
                 break
-        print("recv: done")
-        print("close")
+        # print("recv: done")
+        # print("close")
         s.close()
     except Exception as e:
-        print("http_get failed:", e)
-    dur_total = time.time() - time_start
-    return (success, dur_send, dur_recv, dur_total, len_recv, bps_send, bps_recv)
+        print("http_get Exception:", e)
+        success = False
+    dur_total_s = (time.ticks_ms() - time_start_ms ) / 1000
+    dur_send_s = dur_send_ms / 1000
+    dur_recv_s = dur_recv_ms / 1000
+    if success:
+        print("http_get succeeded:", end="")
+    else:
+        print("http_get failed:", end="")
+    print(len_recv, "bytes received in", dur_recv_s, "s ->", bps_recv, "bps")
+    return (success, dur_send_ms/ 1000, dur_recv_ms/1000, dur_total_s, len_recv, bps_send, bps_recv)
+
 
 # def http_gets(url = def_url, count=1):
 #     for c in range(0, count):
@@ -70,8 +107,9 @@ def http_gets(url = def_url, count=1):
 
 if __name__ == "__main__":
     # http_get()
+    http_get(kb=0)
     # http_get('http://micropython.org/ks/test.html')
-    http_gets('http://detectportal.firefox.com/')
+    # http_gets('http://detectportal.firefox.com/')
     # http_gets('http://192.168.178.1/', 1) # router login page
     # http_get('http://ftp.snt.utwente.nl/pub/docs/rfc/rfc1498.json') # 1000 B
     # http_get.http_gets('http://ftp.snt.utwente.nl/pub/docs/rfc/rfc1535.html') #   10 KiB
