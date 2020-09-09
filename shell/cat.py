b1ada9c23c754cf8e9341baaa87a320e135b6993
Peter Putz <peter@pycom.io>
2020-09-09 10:41:46 +0200

    m

--- shell/cat.py
+++ shell/cat.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     # cat("/flash/test/test.bin.up")
     # cat("/flash/test/http_get.recv")
     #cat("/flash/pybytes_config.json")
+    cat("/flash/boot.py")
     # cat("/flash/main.py")
     # cat("/flash/log_GPy_240ac4c7b250.log")
     cat("/flash/OTA_VERSION.py")
