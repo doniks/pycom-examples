65948e35779cfd10f40c1d56ae0337adf9e0b379
Peter Putz <peter@pycom.io>
2020-09-09 10:28:53 +0200

    whoami: sqns

--- shell/whoami.py
+++ shell/whoami.py
@@ -9,6 +9,10 @@ def whoami(verbose=False):
         for attr in dir(os.uname()):
             if attr[0] != '_':
                 print(attr, getattr(os.uname(),attr))
+        if os.uname().nodename == 'GPy' or os.uname().nodename == 'FiPy':
+            from network import LTE
+            lte = LTE()
+            print('Sequans FW', lte.send_at_cmd('ATI1').split('\r\n')[2])
 
 if __name__ == "__main__":
     whoami(True)
