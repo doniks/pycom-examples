2fa16cf172303f8c4741e0ad987117f9e3f2198a
Peter Putz <peter@pycom.io>
2020-09-09 10:43:15 +0200

    eth: refactor reconnect() etc

--- eth.py
+++ eth.py
@@ -1,69 +1,83 @@
 from network import ETH
 import time
+import os
+import machine
+import binascii
 
-def eth_connect(ip_mask_gw_dns=None):
-    eth = ETH(hostname=binascii.hexlify(machine.unique_id()))
-    eth.hostname(binascii.hexlify(machine.unique_id()) + "." + os.uname().sysname + "." + "eth")
-    print("mac", binascii.hexlify(eth.mac()))
-    # (ip, subnet_mask, gateway, DNS_server)
-    # eth.ifconfig(config=('192.168.0.107', '255.255.255.0', '192.168.0.1', '192.168.0.1')) # use this!
-    # eth.ifconfig(config=('192.168.0.107', '255.255.255.0', '192.168.0.1', '8.8.8.8')) # doesn't work?
-    #### eth.ifconfig(config=('10.107.107.107', '255.0.0.0', '10.0.103.1', '10.0.103.1'))
-    if ip_mask_gw_dns:
-        eth.ifconfig(config=ip_mask_gw_dns)
-    if eth.isconnected():
+e = None
+
+def init():
+    global e
+    if e is None:
+        name = os.uname().sysname.lower() + '-' + binascii.hexlify(machine.unique_id()).decode("utf-8")[-4:]
+        e = ETH(hostname=name) # binascii.hexlify(machine.unique_id()))
+        print(name, "eth.py")
+        print("eth mac", binascii.hexlify(e.mac()))
+    return
+
+def reconnect(ip_mask_gw_dns=None):
+    init()
+    if e.isconnected():
         print("already connected, deinit first")
-        eth.deinit()
-        while eth.isconnected():
+        e.deinit()
+        while e.isconnected():
             # wait for connection to go down
             pass
             # print(eth.isconnected())
         print("eth re-init")
-        eth.init()
+        e.init()
+    return connect(ip_mask_gw_dns)
+
+def connect(config=None):
+    init()
+    if e.isconnected():
+        print('already connected')
+        return True
+    # (ip, subnet_mask, gateway, DNS_server)
+    # eth.ifconfig(config=('192.168.0.107', '255.255.255.0', '192.168.0.1', '192.168.0.1')) # use this!
+    # eth.ifconfig(config=('192.168.0.107', '255.255.255.0', '192.168.0.1', '8.8.8.8')) # doesn't work?
+    #### eth.ifconfig(config=('10.107.107.107', '255.0.0.0', '10.0.103.1', '10.0.103.1'))
+    if config:
+        if config.__class__.__name__ == 'tuple':
+            pass
+        else:
+            if config == '192.168.0.107' or config == 107:
+                print('guessing config for', config)
+                config = ('192.168.0.107', '255.255.255.0', '192.168.0.1', '192.168.0.1')
+        print("using fixed ip config:", config)
+        e.ifconfig(config=config)
     print("connecting eth ...")
     i = 0
     for i in range(0,50):
-        if eth.isconnected():
+        if e.isconnected():
             print("... eth connected")
             break
         else:
-            print(i, "waiting for connection", eth.ifconfig()) #, end=" ")
+            print(i, "waiting for connection", e.ifconfig()) #, end=" ")
             time.sleep(0.5)
         i+=1
 
-    if eth.isconnected():
-        print("hostname", eth.hostname())
-        print("isconnected", eth.isconnected())
-        print("ifconfig", eth.ifconfig()) # (ip, subnet_mask, gateway, DNS_server)
+    if e.isconnected():
+        print("hostname", e.hostname())
+        print("isconnected", e.isconnected())
+        print("ifconfig", e.ifconfig()) # (ip, subnet_mask, gateway, DNS_server)
         return True
     else:
         print("FAILED to connect eth")
         return False
 
-def eth_ifconfig():
-    from network import ETH
-    eth = ETH()
-    print("ifconfig", eth.ifconfig()) # (ip, subnet_mask, gateway, DNS_server)
+def ifconfig():
+    init()
+    print("ifconfig", e.ifconfig()) # (ip, subnet_mask, gateway, DNS_server)
 
-def eth_disconnect():
-    from network import ETH
-    eth = ETH()
-    eth.deinit()
+def disconnect():
+    if e:
+        e.deinit()
 
-if True: # __name__ == "__main__":
-    import os
-    import machine
-    import binascii
-    import pycom
-    id = binascii.hexlify(machine.unique_id())
-    print(os.uname().sysname, id, "eth.py")
-    pycom.heartbeat(False)
-    pycom.rgbled(0xffcc00) # yellow
+if __name__ == "__main__":
+    config = None
     # config=('192.168.0.1', '255.255.255.0', '192.168.0.1', '192.168.0.1')
-    # if id == b'840d8e120b30':
-    #     print("I'm the other guy")
-    config = ('192.168.0.107', '255.255.255.0', '192.168.0.1', '192.168.0.1')
-    if not eth_connect(config):
-        import machine
-        machine.reset()
-    pycom.rgbled(0x00001a) # faint blue
+    # config = ('192.168.0.107', '255.255.255.0', '192.168.0.1', '192.168.0.1')
+    # connect(config)
+    reconnect(107)
+    # connect()
