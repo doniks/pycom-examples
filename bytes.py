298f7b849a7475072976285f59f7d1f058665b25
Peter Putz <peter@pycom.io>
2020-09-09 10:26:23 +0200

    b

--- bytes.py
+++ bytes.py
@@ -5,6 +5,20 @@ import os
 def int_to_bytes(i, len):
     return i.to_bytes(len, "big")
 
+def bytes_to_int(b):
+    return int.from_bytes(b, "big")
+
+def str_to_bytes(s):
+    return bytes(s, "utf-8")
+
+def bytes_to_str(b):
+    return b.decode('utf-8')
+
+
+# An easy way that I was able to do a variable length when packing a string is:
+# pack('{}s'.format(len(string)), string)
+# when unpacking it is kind of the same way
+# unpack('{}s'.format(len(data)), data)
 
 if __name__ == "__main__":
 
@@ -18,5 +32,6 @@ if __name__ == "__main__":
     print()
 
     b = os.urandom(3)
-    i = int.from_bytes(b, "big")
+    #i = int.from_bytes(b, "big")
+    i = bytes_to_int(b)
     print("bytes to int:", binascii.hexlify(b), "->", hex(i), "=", i)
