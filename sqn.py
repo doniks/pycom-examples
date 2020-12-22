f95cc6e5584b51e72e7caf704c083bb5dc86a505
Peter Putz <peter@pycom.io>
2020-12-22 14:01:57 +0100

    m

--- sqn.py
+++ sqn.py
@@ -20,8 +20,9 @@ def init(br=None):
             # not initialized and no br specified, so, we guess
             return init(921600) or init(115200)
     else:
-        p=pins[os.uname().sysname]
-        print('init(', br, ')', p)
+        s = os.uname().sysname
+        p=pins[s]
+        print('init(', br, ')', s, p)
         u = UART(1, baudrate=br, pins=p, timeout_chars=10)
         try:
             at()
@@ -113,8 +114,25 @@ if __name__ == '__main__':
     init()
     at('AT', verbose=True)
     smod()
-
-    # at('AT+SMOD?')
-    # at('AT+BMOD?')
-    # at('AT+SMSWBOOT=?', verbose=True)
-    # help(True)
+    if False:
+        # which mode are we in right now
+        at('AT+SMOD?') # 2
+        at('AT+BMOD?') # FFF
+        # which mode is configured for next boot
+        at('AT+SMSWBOOT?')
+        # change mode for next boot
+        at('AT+SMSWBOOT=?', verbose=True)
+        # +SMSWBOOT: mode[,reboot]
+        #   mode: 0=FFH, 1=FFF, 2=UPDATER, 3=RECOVERY
+        #   reboot: 0=Do not reboot, 1=reboot
+        at('AT+SMSWBOOT=0', verbose=True) # FFH
+        at('AT+SMSWBOOT=1', verbose=True) # FFF
+        at('AT+SMSWBOOT=2', verbose=True) # UPDATER
+        at('AT+SMSWBOOT=3', verbose=True) # RECOVERY
+        at('AT^RESET', verbose=True)
+        at()
+        help(True)
+        help(verbose=True)
+    at("ATI1")
+    # UE:5.0.0.0d;att:5.1.0.0d
+    # LR5.1.1.0-39529
