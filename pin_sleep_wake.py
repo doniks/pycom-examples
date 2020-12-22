19880ce7b35890ddc308f19dc4d9bc853cf867dd
Peter Putz <peter@pycom.io>
2020-12-22 13:56:51 +0100

    m

--- pin_sleep_wake.py
+++ pin_sleep_wake.py
@@ -5,20 +5,21 @@ import time
 
 pins = ["P2", "P3", "P4"]
 #pins = ["P4"]
+pins = ["P13"]
 # P2, P3, P4, P6, P8 to P10 and P13 to P23
 
-wake_on_pull_up = True
+wake_on_rise = True
 
 s = 60000
 
-if wake_on_pull_up:
-    print("Pull", pins, "down. Device will wake when any is pulled high")
+if wake_on_rise:
+    print("Device will wake when any of", pins, "is raised high")
     wake_pins = []
     for p in pins:
         wake_pins += [Pin(p, mode=Pin.IN, pull=Pin.PULL_DOWN)]
     machine.pin_sleep_wakeup(wake_pins, machine.WAKEUP_ANY_HIGH, True)
 else:
-    print("Pull", pins, "high. Device will wake when all are pulled low")
+    print("Device will wake when all of", pins, "are pulled low")
     wake_pins = []
     for p in pins:
         wake_pins += [Pin(p, mode=Pin.IN, pull=Pin.PULL_UP)]
