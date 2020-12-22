e5db087dc0d30a0236b9c0f8ef4a9267a358e312
Peter Putz <peter@pycom.io>
2020-12-22 13:56:37 +0100

    m

--- pin.py
+++ pin.py
@@ -6,18 +6,30 @@ from machine import Pin
 import pycom
 import time
 
-out_p = Pin("P12", mode=Pin.OUT)
+out_p = Pin("P11", mode=Pin.OUT)
 out_p.value(1)
 
 in_p = Pin("P13", mode=Pin.IN, pull=Pin.PULL_UP)
 
 pycom.heartbeat(False)
 
+
+def low():
+    print("_", end="")
+    out_p(0)
+    pycom.rgbled(0x0a000a)
+    time.sleep_ms(1000)
+
+def high():
+    print("-", end="")
+    out_p(1)
+    pycom.rgbled(0x220022)
+    time.sleep_ms(1000)
+
 while True:
     if in_p():
-        print("X", end="")
-        pycom.rgbled(0x002200)
+        high()
     else:
-        print("_", end="")
-        pycom.rgbled(0x111100)
+        low()
+        high()
     time.sleep_ms(100)
