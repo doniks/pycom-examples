eb4b07d1c99e229cbf1392370c17cfef1b3a20e8
Peter Putz <peter@pycom.io>
2020-12-22 13:55:10 +0100

    m

--- bin2int.py
+++ bin2int.py
@@ -9,6 +9,8 @@ def int2hexstring(i):
     #return "%x"%i # 10 -> a
     return hex(i) # 10 -> 0xa
 
+def int2binstring(i):
+    return "{0:b}".format(i)
 
 
 print(2)
@@ -18,3 +20,4 @@ print(binstring2int("0101"))
 print(int("0xa"))
 print(hexstring2int("a"))
 print(int2hexstring(10))
+print(int2binstring(12)) # 1100
