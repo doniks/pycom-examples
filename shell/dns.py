0d49f3683cb09f24e02fe9c9bfd983331ef7e980
Peter Putz <peter@pycom.io>
2020-12-22 14:02:51 +0100

    pycom sims

--- shell/dns.py
+++ shell/dns.py
@@ -1,6 +1,6 @@
 import time
 
-def dns(host='pycom.io', attempts=1):
+def dns(host="mqtt.pybytes.pycom.io", attempts=1):
     import socket
     import os
 
@@ -17,16 +17,24 @@ def dns(host='pycom.io', attempts=1):
         raise Exception(time.time(), "dns lookup failed")
 
 
-def dns_test(attempts=3):
-    for name in range(ord('a'), ord('n')):
-        t = time.ticks_ms()
-        h = chr(name)+'.root-servers.net'
-        dns(h, attempts=attempts)
-        print('h', h, 'seconds:', (time.ticks_ms() - t) / 1000 )
+
+def _dns_test(hostname, attempts=3):
+    t = time.ticks_ms()
+    dns(hostname, attempts=attempts)
+    print('hostname', hostname, 'seconds:', (time.ticks_ms() - t) / 1000 )
+
+def dns_test(index=None, attempts=3):
+    if index is not None:
+        name = ord('a') + index
+        _dns_test(chr(name)+'.root-servers.net', attempts)
+    else:
+        for name in range(ord('a'), ord('n')):
+            _dns_test(chr(name)+'.root-servers.net', attempts)
 
 if __name__ == "__main__":
     import binascii
     import machine
     print(os.uname().sysname, binascii.hexlify(machine.unique_id()), "dns.py")
     # dns('www.pycom.io')
-    dns_test(attempts=100)
+    dns('pybytes.pycom.io')
+    # dns_test(attempts=100)
