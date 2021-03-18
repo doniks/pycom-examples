756a93abd792a3db9e055ccb6c2ce46e52ea9c5f
Peter Putz <peter@pycom.io>
2021-03-18 19:00:29 +0100

    m

--- shell/hexdump.py
+++ shell/hexdump.py
@@ -1,5 +1,5 @@
 import binascii
-def hexdump(file=None, buf=None, print_ascii=True, head=None):
+def hexdump(file=None, buf=None, print_ascii=True, print_bin=False, head=None):
     if file is not None:
         f = open(filename, 'r')
         buf = f.read()
@@ -16,7 +16,10 @@ def hexdump(file=None, buf=None, print_ascii=True, head=None):
         else:
             raise Exception("don't know what to do with", type(b))
         # pretty print one byte
-        print("{:02x} ".format(o), end="")
+        if print_bin:
+            print("{:08b} ".format(o), end="")
+        else:
+            print("{:02x} ".format(o), end="")
         if (0 <= o and o <= 0x1f) or o in [0x23, 0x7f, 0x81, 0x8d, 0x8f, 0x90, 0x9d, 0xa0, 0xad]: # c == '\n' or c == '\r' or (): #o in [0, 1, 2, 3, 4, 0x7f]:
             # non-printable characters
             # there are probably many more ... you find one, you fix it
@@ -60,5 +63,6 @@ if __name__ == "__main__":
     # hexdump("/flash/test/test.bin.up")
     # hexdump("/flash/test/http_get.recv")
     # hexdump("/flash/up41065.elf", head=2000)
-    hexdump(buf='ever\r\n')
-    hexdump(buf=b'1234ff')
+    # hexdump(buf='ever\r\n')
+    # hexdump(buf=b'1234ff')
+    hexdump(buf=buf,print_bin=True, print_ascii=False)
