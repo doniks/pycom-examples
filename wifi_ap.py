141992e9cc07a9396b7e7efc01bec4bd0d231c38
Peter Putz <peter@pycom.io>
2021-08-24 10:09:20 +0200

    m

--- wifi_ap.py
+++ wifi_ap.py
@@ -1,10 +1,21 @@
-import machine
 import os
 import binascii
+import machine
+import pycom
+from network import WLAN
 uid = binascii.hexlify(machine.unique_id())
 name = os.uname().sysname.lower() + '-' + uid.decode("utf-8")[-4:]
 print(os.uname().sysname, uid, name, "wlan_ap.py")
 
-from network import WLAN
 wlan = WLAN(mode=WLAN.AP, ssid=name, auth=(WLAN.WPA2, "8letters"))
 print(wlan.ifconfig(id=1)) #id =1 signifies the AP interface
+
+if False:
+    print('wifi_on_boot', pycom.wifi_on_boot())
+    print('wifi_mode_on_boot', pycom.wifi_mode_on_boot())
+    print('AP', WLAN.AP)
+    print('wifi_ssid_ap', pycom.wifi_ssid_ap())
+    pycom.wifi_on_boot(True)
+    pycom.wifi_mode_on_boot(WLAN.AP)
+    pycom.wifi_ssid_ap('peter2-test')
+    machine.reset()
